fef0a569c08764602efd5af37584b7175c2cd9aa
Aleksi <aleksi.suoranta@gmail.com>
2025-06-22 00:31:57 +0300

    Clamp paddle to stay inside the arena

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::{color::palettes::css::ORANGE, prelude::*};
 
 const PLAYER_MOVE_SPEED: f32 = 200.0;
+const PLAYER_PADDLE_LENGTH: f32 = 50.;
 const BLOCK_THICKNESS: f32 = 10.;
 const LEFT_WALL_X: f32 = -400.;
 const RIGHT_WALL_X: f32 = 400.;
@@ -74,7 +75,7 @@ fn add_paddle(
         Transform::from_xyz(0., -300., 0.0),
         Velocity(Vec2 { x: 0.0, y: 0.0 }),
         Mesh2d(meshes.add(Rectangle::from_size(Vec2 {
-            x: 50.,
+            x: PLAYER_PADDLE_LENGTH,
             y: BLOCK_THICKNESS,
         }))),
         MeshMaterial2d(materials.add(Color::from(ORANGE))),
@@ -89,17 +90,39 @@ fn move_moving(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity)>) {
 
 fn handle_input(
     keys: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Velocity, With<PlayerPaddle>>,
+    mut query: Query<(&mut Velocity, &mut Transform), With<PlayerPaddle>>,
 ) {
     let mut vel: f32 = 0.0;
+    let (mut velocity_comp, mut transform_comp) = query.single_mut().unwrap();
     if keys.pressed(KeyCode::KeyA) {
         vel -= PLAYER_MOVE_SPEED;
     }
     if keys.pressed(KeyCode::KeyD) {
         vel += PLAYER_MOVE_SPEED
     }
-
-    for mut vel_comp in &mut query.iter_mut() {
-        vel_comp.0.x = vel;
+    let clamped = clamp_paddle_loc(transform_comp);
+    if clamped {
+        velocity_comp.0.x = 0.;
+    }else {
+          velocity_comp.0.x = vel;
     }
+}
+
+fn clamp_paddle_loc(mut transform: Mut<'_, Transform>) -> bool {
+    let mut pos = transform.translation;
+    let half_paddle_length = PLAYER_PADDLE_LENGTH /2.;
+    let half_block_thickness = BLOCK_THICKNESS / 2.;
+    let left_max = LEFT_WALL_X + half_block_thickness + half_paddle_length;
+    if pos.x < left_max {
+        pos.x = left_max;
+        transform.translation = pos;
+        return true;
+    }
+    let right_max = RIGHT_WALL_X - half_block_thickness - half_paddle_length;
+    if pos.x > right_max {
+        pos.x = right_max;
+        transform.translation = pos;
+        return true;
+    }
+    return false;
 }
