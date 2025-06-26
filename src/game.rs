b05e47cd2d263000f6b7f09a515165bfe1618c6b
Aleksi <aleksi.suoranta@gmail.com>
2025-06-26 12:43:41 +0300

    Refactor game ui into a separate module

--- src/game.rs
+++ src/game.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 mod ball;
 mod common;
+mod game_ui;
 mod physics;
 mod player;
 mod tile;
@@ -12,9 +13,6 @@ use common::system_sets::*;
 #[derive(Resource, DerefMut, Deref)]
 struct Score(u32);
 
-#[derive(Component)]
-struct ScoreTextUI;
-
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
         DefaultPlugins,
@@ -23,36 +21,8 @@ pub(super) fn plugin(app: &mut App) {
         wall::plugin,
         tile::plugin,
         physics::plugin,
+        game_ui::plugin,
     ))
     .configure_sets(FixedUpdate, InputSet.before(PhysicsSet))
-    .insert_resource(Score(0))
-    .add_systems(Startup, (add_ui))
-    .add_systems(Update, update_score_ui);
-}
-
-fn add_ui(mut commands: Commands) {
-    commands.spawn((
-        Node {
-            position_type: PositionType::Absolute,
-            top: Val::Px(5.0),
-            left: Val::Px(5.0),
-            ..default()
-        },
-        children![(
-            ScoreTextUI,
-            Text::new("Score: 0"),
-            TextShadow::default(),
-            Node {
-                position_type: PositionType::Relative,
-                ..default()
-            }
-        )],
-    ));
-}
-
-fn update_score_ui(score_res: Res<Score>, mut text_query: Single<&mut Text, With<ScoreTextUI>>) {
-    if !score_res.is_changed() {
-        return;
-    }
-    text_query.0 = format!("Score: {}", score_res.0);
+    .insert_resource(Score(0));
 }
