a5b0ca4cd65ddd3b8069821e030dedddfbe4c9cd
Aleksi <aleksi.suoranta@gmail.com>
2025-07-20 11:47:28 +0300

    Remove all unused imports

--- webapp/src/app/app.ts
+++ webapp/src/app/app.ts
@@ -1,14 +1,10 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GameLoaderService } from './game-loader.service.js';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [
-    AsyncPipe
-  ]
 })
 export class App  {
   gameLoader = inject(GameLoaderService)
